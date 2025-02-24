c6f03e63932f201ed38d4541662a888fae76e17f
dianafarcasiu <dianafarcasiu24@gmail.com>
2025-02-24 16:16:38 +0200

    logo path fix

--- src/components/ui/Logo.jsx
+++ src/components/ui/Logo.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 function Logo() {
   return (
     <Link to="/">
-      <img className="h-16" src="/cocktail-logo-2.png" alt="Logo" />
+      <img
+        className="h-16"
+        src={`${import.meta.env.BASE_URL}cocktail-logo-2.png`}
+        alt="Logo"
+      />
     </Link>
   );
 }
